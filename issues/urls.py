d6c8c000d347b0a9235a0726009a172eb3c3019e
timothythomas1 <tuxes-parlay0s@icloud.com>
2022-10-06 20:37:56 -0700

    Added pages views and urls. Added some issues views and urls.

--- issues/urls.py
+++ issues/urls.py
@@ -1,3 +1,19 @@
 from django.urls import path
+from .views import (
+    IssueListView,
+    IssueDetailView,
+    IssueCreateView,
+    IssueDeleteView,
+    IssueUpdateView,
+    # DraftIssueListView,
+    # ArchivedIssueListView,
+    # PublishedIssueListView
+)
 
-urlpatterns = []
+urlpatterns = [
+    path('', IssueListView.as_view(), name='list'),
+    path('<int:pk>', IssueDetailView.as_view(), name='detail'),
+    path('new/', IssueCreateView.as_view(), name='new'),
+    path('<int:pk>/edit/', IssueUpdateView.as_view(), name='edit'),
+    path('<int:pk>/delete/', IssueDeleteView.as_view(), name='confirm_delete'),
+]
