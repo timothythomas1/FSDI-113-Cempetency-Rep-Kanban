346bb1a0541b46d153a2b74629df0826d987401f
timothythomas1 <tuxes-parlay0s@icloud.com>
2022-10-07 05:15:46 -0700

    Fixed delete issues

--- issues/urls.py
+++ issues/urls.py
@@ -4,7 +4,7 @@ from .views import (
     IssueDetailView,
     IssueCreateView,
     IssueDeleteView,
-    IssueUpdateView,
+    IssueUpdateView
     # DraftIssueListView,
     # ArchivedIssueListView,
     # PublishedIssueListView
@@ -12,8 +12,8 @@ from .views import (
 
 urlpatterns = [
     path('', IssueListView.as_view(), name='list'),
-    path('<int:pk>', IssueDetailView.as_view(), name='detail'),
+    path('<int:pk>', IssueDetailView.as_view(), name='issue_detail'),
     path('new/', IssueCreateView.as_view(), name='new'),
     path('<int:pk>/edit/', IssueUpdateView.as_view(), name='edit'),
-    path('<int:pk>/delete/', IssueDeleteView.as_view(), name='confirm_delete'),
+    path('<int:pk>/delete/', IssueDeleteView.as_view(), name='delete'),
 ]
--- issues/views.py
+++ issues/views.py
@@ -3,39 +3,39 @@ from django.views.generic.edit import DeleteView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin# This is needed for object oriented programming. A class should only inherit one class. A Mixin is another class that can be inherited from allwoing for a class to inherit more classes essentially.
 from django.urls import reverse_lazy
 from .models import Issue
-
 class IssueListView(ListView):
     template_name = "issues/list.html"
     model = Issue
 
 class IssueDetailView(DetailView):
-    template_name = "issues/detail.html"
+    template_name = "issues/issue_detail.html"
     model = Issue
 
 class IssueCreateView(LoginRequiredMixin, CreateView):
     template_name = "issues/new.html" 
     model = Issue
-    fields = ["title", "subtitle", "body"]
+    fields = ["assignee", "title", "summary", "description",  "impact", "urgency", "status"]
 
     def form_valid(self, form): # This overrides the default form_valid() method of Django 
-        form.instance.author = self.request.user # Allows the Author to request the user model.
+        form.instance.requester = self.request.user # Allows the requester to request the user model.
         return super().form_valid(form) # Super() calls the form method
 
 class IssueDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView): # REMEMBER, Mixin order matters for validations.
     model = Issue
     # success_url ="/issues" # You can specify success urlurl to redirect after successfully
-    template_name = "issues/confirm_delete.html" # deleting object
+    template_name = "issues/delete.html" # deleting object
     success_url = reverse_lazy("list") # From Django's urls module. Redirects after successfully deleting object
     # Test for UserPassesTestMixin that the user needs to pass
     def test_func(self):
         issue_obj = self.get_object()
-        return issue_obj.author == self.request.user
+        return issue_obj.requester == self.request.user
 
 class IssueUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):  # REMEMBER, Mixin order matters for validations.
     template_name = "issues/edit.html"
     model = Issue
-    fields = ["title", "summary", "description", "status"] # Fields will be showed on the page
+    success_url = reverse_lazy("list") 
+    fields = ["assignee", "title", "summary", "description",  "impact", "urgency", "status"] # Fields will be showed on the page
     # Test for UserPassesTestMixin that the user needs to pass
     def test_func(self):
         issue_obj = self.get_object()
-        return issue_obj.author == self.request.user
+        return issue_obj.requester == self.request.user
